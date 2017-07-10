3cccded90594bee077dd70005d2d251ad97726cd
Julien Dubois <julien.dubois@gmail.com>
2017-07-10 12:12:16 +0200

    automatic project update

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -104,8 +104,8 @@ module.exports = (options) => {
             new MergeJsonWebpackPlugin({
                 output: {
                     groupBy: [
-                        { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./target/www/i18n/en.json" },
-                        { pattern: "./src/main/webapp/i18n/fr/*.json", fileName: "./target/www/i18n/fr.json" }
+                        { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./i18n/en.json" },
+                        { pattern: "./src/main/webapp/i18n/fr/*.json", fileName: "./i18n/fr.json" }
                         // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
                     ]
                 }
